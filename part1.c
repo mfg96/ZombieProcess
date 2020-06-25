1e6eb6c00a74bd27574ba4bb6320de85d16a7c59
Muhammad Faraz Sohail <43736103+mfg96@users.noreply.github.com>
2020-06-24 22:59:47 -0400

    Add files via upload

---
+++ part1.c
@@ -0,0 +1,42 @@
+/*
+Muhammad Faraz Sohail
+104986019
+Assignment 2
+Part1
+*/
+
+//libraries for desired functions
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+  int status = 0;
+
+  //forking here
+  int id = fork();
+  if (id == 0)
+  {
+    //child
+    printf("The child id is: %d \n", id);
+  }
+  else
+  {
+    //parent
+    clock_t begin = clock(); //start clock time
+    printf("Parent id is: %d \n", id);
+    sleep(60);             //sleep of 1 minute
+    clock_t end = clock(); //end clock time
+    printf("The parent ended in (Clocks Per Sec): %lf \n", ((double)(end - begin)) / CLOCKS_PER_SEC);
+    //wait(&status);
+  }
+
+  exit(0);
+}
---
+++ part2.c
@@ -0,0 +1,43 @@
+/*
+Muhammad Faraz Sohail
+104986019
+Assignment 2
+Part2
+*/
+
+//libraries for desired functions
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("Compiling Part1...\n");
+    system("cc part1.c -o Q1"); //compiling command for part1
+
+    printf("Executing Part1...\n");
+    system("./Q1 &"); //running part1 in background command
+
+    printf("\nAll programs with their states:\n");
+    system("ps -l"); //listing all procs with their states and id command
+
+    printf("\nDefunct programs:\n");
+    system("ps -l|grep defunct"); //Showing defunct/zomibe processes
+
+    printf("\nPIDs of Defunct process of Part1:\n");
+    system("ps -fC Q1 | grep defunct"); //PIDs of part1 defunct process
+
+    printf("\nKilling the Parent process of Zombie process!!\n");
+    system("kill -9 $(ps -fC Q1 | grep defunct | awk '{print $3}')"); //killing parent of zombie process
+
+    printf("\nNow showing the processes list:\n");
+    system("ps -l"); //again showing all the processes with their status info and PIDs
+
+    exit(0);
+}
